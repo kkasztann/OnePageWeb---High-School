daf85d9def4a3bedba6cbfaa76f18ea7d4078b11
Daniel Slowik <kkasztann@gmail.com>
2018-12-10 23:57:50 +0100

    upgraded nav

--- js/main.js
+++ js/main.js
@@ -4,22 +4,33 @@ $(document).ready(function () {
     $("#lightgallery").lightGallery();
 
 
-    // Nav Background
+    // Nav Background, Size
     $(window).scroll(function () {
         if ($(window).scrollTop() >= 120) {
             $('nav').addClass('nav-bg');
+            $('.fa-school').addClass('nav-small');
+            $('.nav-item').addClass('nav-small');
+            $('.navbar-brand').addClass('nav-small');
 
         } else if ($('nav').hasClass('nav-bg')) {
             $('nav').removeClass('nav-bg');
+            $('.fa-school').removeClass('nav-small');
+            $('.nav-item').removeClass('nav-small');
+            $('.navbar-brand').removeClass('nav-small');
         }
     });
 
     if ($('nav').offset().top >= 120) {
         $('nav').addClass('nav-bg');
+        $('.fa-school').addClass('nav-small');
+        $('.nav-item').addClass('nav-small');
+        $('.navbar-brand').addClass('nav-small');
     }
 
 
     // Smooth Scroll
-    var scroll = new SmoothScroll('a[href*="#"]');
+    var scroll = new SmoothScroll('a[href*="#"]', {
+        updateURL: false
+    });
 
 });
