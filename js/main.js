057eb671d09f3872ba428ae23a23e54e26475afc
Daniel Slowik <kkasztann@gmail.com>
2018-12-10 15:14:09 +0100

    created nav

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
+
     $("#lightgallery").lightGallery();
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= 120) {
+            $('nav').addClass('nav-bg');
+
+        } else if ($('nav').hasClass('nav-bg')) {
+            $('nav').removeClass('nav-bg');
+        }
+    });
+
+    if ($('nav').offset().top >= 120) {
+        $('nav').addClass('nav-bg');
+    }
+
+
 });
